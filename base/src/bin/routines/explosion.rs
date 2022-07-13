ff455f2aa716aea4c0828be82ec01af44b6c042d
J Wylie Cullick <wylie@terebinth.is>
2022-07-13 13:09:40 -0500

    ...

--- base/src/bin/routines/explosion.rs
+++ base/src/bin/routines/explosion.rs
@@ -7,28 +7,20 @@ use web_sys::{
     EventTarget, MouseEvent, WebGlBuffer, WebGlProgram,
     WebGlUniformLocation,
 };
-
 use serde_json::{Value};
-
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-
 use std::sync::{Arc, Mutex};
-
 use cgmath::prelude::*;
 use cgmath::Rad;
-
 use std::cell::RefCell;
 use std::rc::Rc;
-use std::time::*;
-
 use std::convert::{TryInto};
-use std::ops::{Add, Sub, AddAssign, SubAssign};
 use std::collections::HashMap;
 use std::sync::atomic::AtomicBool;
-
 use gloo_console::log;
 use std::f32::consts::PI;
+use crate::utils::time_polyfill::Instant;
 
 const AMORTIZATION: f32 = 0.95;
 const LOCALIZED_SCALE : f32 = 0.5;
@@ -38,62 +30,6 @@ const SCALE : f32 = 0.08;
 const HALF : f32 = SCALE / 2.0;
 const STEP : f32 = SCALE / RESOLUTION;
 const NUM_PARTICLES : u32 = 9680;
-
-// https://github.com/rust-lang/rust/issues/48564#issuecomment-698712971
-// std::time invocation causes panic.  There is a comment linked above which solves this
-// with the polyfillish stuff below.
-
-#[cfg(not(target_arch = "wasm32"))]
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Instant(std::time::Instant);
-
-#[cfg(not(target_arch = "wasm32"))]
-impl Instant {
-    pub fn now() -> Self { Self(std::time::Instant::now()) }
-    pub fn duration_since(&self, earlier: Instant) -> Duration { self.0.duration_since(earlier.0) }
-    pub fn elapsed(&self) -> Duration { self.0.elapsed() }
-    pub fn checked_add(&self, duration: Duration) -> Option<Self> { self.0.checked_add(duration).map(|i| Self(i)) }
-    pub fn checked_sub(&self, duration: Duration) -> Option<Self> { self.0.checked_sub(duration).map(|i| Self(i)) }
-}
-
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen(inline_js = r#"
-export function performance_now() {
-  return performance.now();
-}"#)]
-extern "C" {
-    fn performance_now() -> f64;
-}
-
-#[cfg(target_arch = "wasm32")]
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Instant(u64);
-
-#[cfg(target_arch = "wasm32")]
-impl Instant {
-    pub fn now() -> Self { Self((performance_now() * 1000.0) as u64) }
-    pub fn duration_since(&self, earlier: Instant) -> Duration { Duration::from_micros(self.0 - earlier.0) }
-    pub fn elapsed(&self) -> Duration { Self::now().duration_since(*self) }
-    pub fn checked_add(&self, duration: Duration) -> Option<Self> {
-        match duration.as_micros().try_into() {
-            Ok(duration) => self.0.checked_add(duration).map(|i| Self(i)),
-            Err(_) => None,
-        }
-    }
-    pub fn checked_sub(&self, duration: Duration) -> Option<Self> {
-        match duration.as_micros().try_into() {
-            Ok(duration) => self.0.checked_sub(duration).map(|i| Self(i)),
-            Err(_) => None,
-        }
-    }
-}
-
-impl Add<Duration> for Instant { type Output = Instant; fn add(self, other: Duration) -> Instant { self.checked_add(other).unwrap() } }
-impl Sub<Duration> for Instant { type Output = Instant; fn sub(self, other: Duration) -> Instant { self.checked_sub(other).unwrap() } }
-impl Sub<Instant>  for Instant { type Output = Duration; fn sub(self, other: Instant) -> Duration { self.duration_since(other) } }
-impl AddAssign<Duration> for Instant { fn add_assign(&mut self, other: Duration) { *self = *self + other; } }
-impl SubAssign<Duration> for Instant { fn sub_assign(&mut self, other: Duration) { *self = *self - other; } }
-
 
 pub fn explosion()
 {
@@ -126,14 +62,21 @@ pub fn explosion()
     let shader_program = gl.create_program().unwrap();
     gl.attach_shader(&shader_program, &vert_shader);
     gl.attach_shader(&shader_program, &frag_shader);
+
+    let data = r#"
+    ["vPosition", "vVelocity"]
+    "#;
+    let v: Value = serde_json::from_str(data).unwrap();
+    let js_val = wasm_bindgen::JsValue::from_serde(&v).unwrap();
+    gl.transform_feedback_varyings(&shader_program, &js_val, GL::SEPARATE_ATTRIBS);
+
     gl.link_program(&shader_program);
+    
 
 
     let position_data : Arc<Mutex<[f32; (NUM_PARTICLES * 3) as usize]>> = Arc::new(Mutex::new([0.0; (NUM_PARTICLES * 3) as usize]));
     let velocity_data : Arc<Mutex<[f32; (NUM_PARTICLES * 3) as usize]>> = Arc::new(Mutex::new([0.0; (NUM_PARTICLES *3) as usize]));
     let color_data : Arc<Mutex<[f32; (NUM_PARTICLES * 3) as usize]>> = Arc::new(Mutex::new([0.0; (NUM_PARTICLES * 3) as usize]));
-
-    
 
     for i in 0..NUM_PARTICLES {
         let vec3i : usize = (i as usize) * 3;
@@ -142,16 +85,13 @@ pub fn explosion()
         position_data.lock().unwrap()[vec3i + 1] = (js_sys::Math::random() as f32) * LOCALIZED_SCALE - CORRECTION;
         position_data.lock().unwrap()[vec3i + 2] = (js_sys::Math::random() as f32) * LOCALIZED_SCALE - CORRECTION;
 
-
-
-        // position_data.lock().unwrap()[vec3i] = (js_sys::Math::random() as f32);
-        // position_data.lock().unwrap()[vec3i + 1] = (js_sys::Math::random() as f32);
-        // position_data.lock().unwrap()[vec3i + 2] = (js_sys::Math::random() as f32);
-
         color_data.lock().unwrap()[vec3i] = js_sys::Math::random() as f32;
         color_data.lock().unwrap()[vec3i + 1] = js_sys::Math::random() as f32;
         color_data.lock().unwrap()[vec3i + 2] = js_sys::Math::random() as f32;
     }
+
+    let vertex_array_a = Arc::new(gl.create_vertex_array().unwrap());
+    gl.bind_vertex_array(Some(&vertex_array_a));
 
     let position_buffer_a = Arc::new(gl.create_buffer().unwrap());
     gl.bind_buffer(GL::ARRAY_BUFFER, Some(&position_buffer_a));
@@ -160,53 +100,110 @@ pub fn explosion()
     gl.vertex_attrib_pointer_with_i32(0, 3, GL::FLOAT, false, 0, 0);
     gl.enable_vertex_attrib_array(0);
 
-    // let velocity_buffer_a = Arc::new(gl.create_buffer().unwrap());
-    // gl.bind_buffer(GL::ARRAY_BUFFER, Some(&velocity_buffer_a));
-    // let velocity_data_js = js_sys::Float32Array::from(velocity_data.lock().unwrap().as_slice());
-    // gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &velocity_data_js, GL::STREAM_COPY);
-    // gl.vertex_attrib_pointer_with_i32(1, 3, GL::FLOAT, false, 0, 0);
-    // gl.enable_vertex_attrib_array(1);
+    let velocity_buffer_a = Arc::new(gl.create_buffer().unwrap());
+    gl.bind_buffer(GL::ARRAY_BUFFER, Some(&velocity_buffer_a));
+    let velocity_data_js = js_sys::Float32Array::from(velocity_data.lock().unwrap().as_slice());
+    gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &velocity_data_js, GL::STREAM_COPY);
+    gl.vertex_attrib_pointer_with_i32(1, 3, GL::FLOAT, false, 0, 0);
+    gl.enable_vertex_attrib_array(1);
 
-    // let color_buffer = Arc::new(gl.create_buffer().unwrap());
-    // gl.bind_buffer(GL::ARRAY_BUFFER, Some(&color_buffer));
-    // let color_data_js = js_sys::Float32Array::from(color_data.lock().unwrap().as_slice());
-    // gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &color_data_js, GL::STATIC_DRAW);
-    // gl.enable_vertex_attrib_array(2);
+    let color_buffer = Arc::new(gl.create_buffer().unwrap());
+    gl.bind_buffer(GL::ARRAY_BUFFER, Some(&color_buffer));
+    let color_data_js = js_sys::Float32Array::from(color_data.lock().unwrap().as_slice());
+    gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &color_data_js, GL::STATIC_DRAW);
+    gl.vertex_attrib_pointer_with_i32(2, 3, GL::FLOAT, false, 0, 0);
+    gl.enable_vertex_attrib_array(2);
+
+    gl.bind_buffer(GL::ARRAY_BUFFER, None);
+
+    // Transform feedback handles output 
+    // https://github.com/tsherif/webgl2examples/blob/master/particles.html
 
     let transform_feedback_a = Arc::new(gl.create_transform_feedback().unwrap());
+    gl.bind_transform_feedback(GL::TRANSFORM_FEEDBACK, Some(&transform_feedback_a));
+    gl.bind_buffer_base(GL::TRANSFORM_FEEDBACK_BUFFER, 0, Some(&position_buffer_a));
+    gl.bind_buffer_base(GL::TRANSFORM_FEEDBACK_BUFFER, 1, Some(&velocity_buffer_a));
+
+    let vertex_array_b = Arc::new(gl.create_vertex_array().unwrap());
+    gl.bind_vertex_array(Some(&vertex_array_b));
+
+    let position_buffer_b = Arc::new(gl.create_buffer().unwrap());
+    gl.bind_buffer(GL::ARRAY_BUFFER, Some(&position_buffer_b));
+    gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &position_data_js, GL::STREAM_COPY);
+    gl.vertex_attrib_pointer_with_i32(0, 3, GL::FLOAT, false, 0, 0);
+    gl.enable_vertex_attrib_array(0);
+
+    let velocity_buffer_b = Arc::new((gl.create_buffer().unwrap()));
+    gl.bind_buffer(GL::ARRAY_BUFFER, Some(&velocity_buffer_b));
+    gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &velocity_data_js, GL::STREAM_COPY);
+    gl.vertex_attrib_pointer_with_i32(1, 3, GL::FLOAT, false, 0, 0);
+    gl.enable_vertex_attrib_array(1);
+
+    gl.bind_buffer(GL::ARRAY_BUFFER, Some(&color_buffer));
+    gl.vertex_attrib_pointer_with_i32(2, 3, GL::FLOAT, false, 0, 0);
+    gl.enable_vertex_attrib_array(2);
+
+    gl.bind_buffer(GL::ARRAY_BUFFER, None);
+
+    let transform_feedback_b = Arc::new(gl.create_transform_feedback().unwrap());
+    gl.bind_transform_feedback(GL::TRANSFORM_FEEDBACK, Some(&transform_feedback_b));
+    gl.bind_buffer_base(GL::TRANSFORM_FEEDBACK_BUFFER, 0, Some(&position_buffer_b));
+    gl.bind_buffer_base(GL::TRANSFORM_FEEDBACK_BUFFER, 1, Some(&velocity_buffer_b));
+
+    gl.bind_vertex_array(None);
+    gl.bind_transform_feedback(GL::TRANSFORM_FEEDBACK, None);
+
+    let mut current_vertex_array : Arc<Mutex<_>> = Arc::new(Mutex::new((*vertex_array_a).clone()));
+    let mut current_transform_feedback : Arc<Mutex<_>> = Arc::new(Mutex::new((*transform_feedback_b).clone()));
+    
+    
+
+    
+
+    
 
     gl.use_program(Some(&shader_program));
-
-
-
-
+    let mut switch = Arc::new(Mutex::new(AtomicBool::new(true)));
 
     gl.clear_color(0.98, 0.983, 0.992, 1.0);
 
     let render_loop_closure = Rc::new(RefCell::new(None));
     let alias_rlc = render_loop_closure.clone();
     *alias_rlc.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+        
+        
         gl.clear(GL::COLOR_BUFFER_BIT);
 
 
-        // let position_buffer_a = Arc::new(gl.create_buffer().unwrap());
-        // gl.bind_buffer(GL::ARRAY_BUFFER, Some(&position_buffer_a));
-        // let position_data_js = js_sys::Float32Array::from(position_data.lock().unwrap().as_slice());
-        // gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &position_data_js, GL::STREAM_COPY);
-        // gl.vertex_attrib_pointer_with_i32(0, 3, GL::FLOAT, false, 0, 0);
-        // gl.enable_vertex_attrib_array(0);
+        gl.bind_vertex_array(Some(current_vertex_array.lock().unwrap().as_ref()));
+        gl.bind_transform_feedback(GL::TRANSFORM_FEEDBACK, Some(current_transform_feedback.lock().unwrap().as_ref()));
     
-        // let velocity_buffer_a = Arc::new(gl.create_buffer().unwrap());
-        // gl.bind_buffer(GL::ARRAY_BUFFER, Some(&velocity_buffer_a));
-        // let velocity_data_js = js_sys::Float32Array::from(velocity_data.lock().unwrap().as_slice());
-        // gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &velocity_data_js, GL::STREAM_COPY);
-        // gl.vertex_attrib_pointer_with_i32(1, 3, GL::FLOAT, false, 0, 0);
-        // gl.enable_vertex_attrib_array(1);
-
-
-        // gl.begin_transform_feedback(GL::POINTS);
+        let s = *switch.lock().unwrap().get_mut();
+        // if *current_transform_feedback.lock().unwrap() == *transform_feedback_a.lock().unwrap() {
+        if !s {
+            gl.bind_buffer_base(GL::TRANSFORM_FEEDBACK_BUFFER, 0, Some(&position_buffer_a));
+            gl.bind_buffer_base(GL::TRANSFORM_FEEDBACK_BUFFER, 1, Some(&velocity_buffer_a));
+        } else {
+            gl.bind_buffer_base(GL::TRANSFORM_FEEDBACK_BUFFER, 0, Some(&position_buffer_b));
+            gl.bind_buffer_base(GL::TRANSFORM_FEEDBACK_BUFFER, 1, Some(&velocity_buffer_b));
+        }
+    
+        gl.begin_transform_feedback(GL::POINTS);
         gl.draw_arrays(GL::POINTS, 0, NUM_PARTICLES as i32);
-        // gl.end_transform_feedback();
+        gl.end_transform_feedback();
+    
+        if s {
+            *current_vertex_array.lock().unwrap() = (*vertex_array_b).clone();
+            *current_transform_feedback.lock().unwrap() = (*transform_feedback_a).clone();
+        } else 
+        {
+            *current_vertex_array.lock().unwrap() = (*vertex_array_a).clone();
+            *current_transform_feedback.lock().unwrap() = (*transform_feedback_b).clone();
+        }
+        *switch.lock().unwrap().get_mut() = !s; 
+        gl.bind_vertex_array(None);
+
+        
 
 
         request_animation_frame(render_loop_closure.borrow().as_ref().unwrap());
